a95f4a33c61cd31433a46cd3961bf7a1edfe30b7
juanpablo cifuentes <juanpcc1@gmail.com>
2026-03-17 01:08:46 +0000

    Try fixing this error: `Runtime TypeError: plugin is not a function. Err

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,11 +1,12 @@
+
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
-import {openAI} from 'genkitx-openai';
+import {openai} from 'genkitx-openai';
 
 export const ai = genkit({
   plugins: [
     googleAI(),
-    openAI(),
+    openai(),
   ],
   model: 'googleai/gemini-2.0-flash',
 });
